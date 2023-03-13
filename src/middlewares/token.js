a4c0b2f34166b1e434ef6286cc03de6fb3786699
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 17:00:50 -0300

    feat(token)add middleware de gerar é validar token.

---
+++ src/middlewares/token.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const jwtCheckUser = {
+
+  generateToken: (email) => {
+    const token = jwt.sign({ email }, JWT_SECRET, {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    });
+    return token;
+  },
+
+  validateToken: (token) => {
+    if (!token) throw new Error('401|Token not found');
+    try {
+      const test = jwt.verify(token, JWT_SECRET);
+      return test;
+    } catch (error) {
+      console.log(error);
+      throw new Error('401|Expired or invalid token');
+    }
+  },
+};
+
+module.exports = jwtCheckUser;
