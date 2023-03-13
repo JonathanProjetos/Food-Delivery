d3e1d9f5b796dfca91b3dbc4f762cf492a823f23
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 19:30:44 -0300

    feat(RouteLogin)Add rota register

--- src/routes/RouteLogin.js
+++ src/routes/RouteLogin.js
@@ -4,5 +4,6 @@ const LoginController = require('../controllers/Login');
 const loginRouter = Router();
 
 loginRouter.post('/login', LoginController.Login);
+loginRouter.post('/register', LoginController.Register);
 
 module.exports = loginRouter;
