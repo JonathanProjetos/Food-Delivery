8bafdf9e0b9f0cea42c20de7af641ffee2af780d
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 20:01:55 -0300

    feat(CheckAcessLogin)Add função para validação do email vindo do token.

--- src/controllers/Login.js
+++ src/controllers/Login.js
@@ -7,6 +7,12 @@ const LoginController = {
     res.status(200).json({ token });
   },
 
+  CheckAcessLogin: async (req, res) => {
+    const { email } = req.email;
+    const message = await LoginServices.CheckAcessLogin(email);
+    res.status(200).json(message);
+  },
+
   Register: async (req, res) => {
     const message = await LoginServices.Register(req.body);
     res.status(200).json(message);
