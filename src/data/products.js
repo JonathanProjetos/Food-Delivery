a604e9d16c8f621035533372ea43ce5020dc2c9f
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 17:00:10 -0300

    feat(products)lista de produtos

---
+++ src/data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: "Pizza",
+    price: "10",
+    description: "A small",
+  },
+  {
+    name: "churrasco",
+    price: "100",
+    description: "A big",
+  },
+  {
+    name: "hambuguer",
+    price: "50",
+    description: "A big",
+  },
+  {
+    name: "sorvete",
+    price: "15",
+    description: "A small",
+  },
+  {
+    name: "bolo",
+    price: "50",
+    description: "A big",
+  },
+  {
+    name: "refrigerante",
+    price: "5",
+    description: "A small",
+  },
+  {
+    name: "cafe",
+    price: "3",
+    description: "A small",
+  },
+  {
+    name: "suco",
+    price: "5",
+    description: "A small",
+  },
+  {
+    name: "cerveja",
+    price: "10",
+    description: "A small",
+  },
+  {
+    name: "vinho",
+    price: "20",
+    description: "A small",
+  },
+  {
+    name: "agua",
+    price: "5",
+    description: "A small",
+  },
+  {
+    name: "sushi",
+    price: "50",
+    description: "A big",
+  },
+  {
+    name: "espaguete",
+    price: "30",
+    description: "A big",
+  },
+  {
+    name: "lasanha",
+    price: "30",
+    description: "A big",
+  }
+]
+
+module.exports = products
