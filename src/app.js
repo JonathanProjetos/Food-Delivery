866944d1ada67480b88f82e6339c0d22f47f0afc
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 19:17:31 -0300

    feat(login)Adicionado rota login

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('express-async-errors');
+const loginRouter = require('./routes/RouteLogin');
+
+
 const app = express();
-
-
 app.use(express.json())
+
+app.use('/', loginRouter);
 
 app.use((err, _req, res, _next) => {
   const [code, message] = err.message.split('|');
