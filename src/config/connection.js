79a45edd3c80fd1e5f8924e18ee2af3824aa86e2
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 16:58:16 -0300

    feat(connection)criando conexão com mongo.

---
+++ src/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+require = ('dotenv/config')
+
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+
+const connectToDataBase = (
+  mongoDataBaseURI = MONGO_DB_URL
+) => mongoose.connect(mongoDataBaseURI);
+
+module.exports = connectToDataBase;
