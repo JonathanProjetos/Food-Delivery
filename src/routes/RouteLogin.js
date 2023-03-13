462dde83207574e6e520765214070c90ba395b1c
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 20:03:16 -0300

    feat(loginRoute)Add rota para validação do corpo de token.

--- src/routes/RouteLogin.js
+++ src/routes/RouteLogin.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const LoginController = require('../controllers/Login');
+const checkToken = require('../middlewares/checkToken');
 
 const loginRouter = Router();
 
 loginRouter.post('/login', LoginController.Login);
+loginRouter.get('/login/validate', checkToken.verifyToken, LoginController.CheckAcessLogin)
 loginRouter.post('/register', LoginController.Register);
 
 module.exports = loginRouter;
