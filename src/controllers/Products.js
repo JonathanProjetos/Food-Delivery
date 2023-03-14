0acfef4142763b893edb82ba19f11d68e0757163
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 23:39:48 -0300

    feat(create)Add método para criar um novo produto.

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -4,9 +4,17 @@ const ProductsServices = require('../services/Products');
 const ProductsController = {
   getAll: async (req, res) => {
     const { email } = req.email;
-
+    
     const allProducts = await ProductsServices.getAll(email);
     res.status(200).json(allProducts);
+  },
+
+  create: async (req, res) => {
+    const { name, price, description } = req.body;
+    const { email } = req.email;
+
+    const newProduct = await ProductsServices.create({ name, price, description, email });
+    res.status(201).json(newProduct);
   }
 }
 
