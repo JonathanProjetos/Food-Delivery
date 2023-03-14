6c158ef8938614d5d7d0827d179a2112f62f81b6
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 23:50:28 -0300

    feat(ProductsServices)Add método create products.

--- src/services/Products.js
+++ src/services/Products.js
@@ -7,6 +7,21 @@ const ProductsServices = {
     const allProducts = await products.find();
     return allProducts;
   },
+
+  create: async (product) => {
+    const { name, price, description, email } = product;
+    if(!email) throw new Error('401|Token not found');
+    
+    const allProducts = await products.find();
+
+    const checkProduct = allProducts.find((product) => product.name.toLowerCase() === name.toLowerCase());
+
+    if(checkProduct) throw new Error('409|Product already exists');
+
+
+    const newProduct = await products.create({ name, price, description });
+    return newProduct;
+  }
 }
 
 module.exports = ProductsServices;
