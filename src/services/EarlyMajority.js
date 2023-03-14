18f813de158dd67ed27258cea912abb87e72b34b
Jonathan Santos <jonathan01347@gmail.com>
2023-03-14 17:23:06 -0300

    feat(EarlyMajority) Add business rule in services

---
+++ src/services/EarlyMajority.js
@@ -0,0 +1,43 @@
+const order = require('../models/orderModel');
+const user = require('../models/userModel');
+const checkQuantityItems = require('../middlewares/checkQuantityItems');
+
+
+const OrdersEarlyMajorityServices = {
+
+  create: async (body) => {
+    const { 
+      name,
+      address,
+      phone,
+      methodPayment,
+      email,
+      orders
+     } = body;
+    
+    // verificando se a quantidade de itens é maior que 5
+    checkQuantityItems.bodyEarlyMajority(orders);
+    // verificando se o token existe
+    if(!email) throw new Error('401|Token not found');
+  
+    const users = await user.find();
+    const getUserId = await user.findOne({ email });
+
+    // verificando a quantidade de usuários cadastrados
+    if(users.length > 1000000) throw new Error('429|Too many requests');
+
+    const result = await order.create({ 
+      name,
+      userId: getUserId.id,
+      address,
+      phone,
+      methodPayment,
+      orders
+    })
+
+    return result;
+  }
+}
+
+
+module.exports = OrdersEarlyMajorityServices;
