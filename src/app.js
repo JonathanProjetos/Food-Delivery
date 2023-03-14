e80536057e302297c4016d6df1efa168e519ae2a
Jonathan Santos <jonathan01347@gmail.com>
2023-03-13 23:36:15 -0300

    feat(products)Add route products

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 const loginRouter = require('./routes/RouteLogin');
+const productsRouter = require('./routes/RouteProducts');
+const MVPRoute = require('./routes/RouteMVP');
 
 
 const app = express();
 app.use(express.json())
 
 app.use('/', loginRouter);
+app.use('/', productsRouter);
 
 app.use((err, _req, res, _next) => {
   const [code, message] = err.message.split('|');
