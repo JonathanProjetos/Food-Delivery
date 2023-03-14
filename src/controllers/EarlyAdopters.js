ef69f6ee2ba76924471a9b783e2a24dce1bda488
Jonathan Santos <jonathan01347@gmail.com>
2023-03-14 18:43:53 -0300

    feat(EarlyAdopters) Add method create in controllers

---
+++ src/controllers/EarlyAdopters.js
@@ -0,0 +1,14 @@
+const Early_Adopters = require('../services/EarlyAdopters');
+
+const ordersEarlyAdoptersController = {
+  create: async (req, res) => {
+    const { email } = req.email;
+    const { body } = req;
+
+    const newOrder = await Early_Adopters.create({ ...body, email });
+    res.status(201).json(newOrder);
+  }
+
+}
+
+module.exports = ordersEarlyAdoptersController;
