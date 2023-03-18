e4b096dccf4d1cb0c722753ef5f4481e8270773f
Jonathan Santos <jonathan01347@gmail.com>
2023-03-18 16:34:32 -0300

    feat(lateMajority)Implemented create in service.

---
+++ src/services/LateMajority.js
@@ -0,0 +1,39 @@
+const order = require('../models/orderModel');
+const user = require('../models/userModel');
+const checkQuantityItems = require('../middlewares/checkQuantityItems');
+
+
+const OrdersLateMajorityServices = {
+
+  create: async (body) => {
+    const { 
+      name,
+      address,
+      phone,
+      methodPayment,
+      email,
+      orders
+     } = body;
+    
+    // verificando se a quantidade de itens é maior que 5
+    checkQuantityItems.bodyLateMajority(orders);
+    // verificando se o token existe
+    if(!email) throw new Error('401|Unauthorized');
+
+    const getUserId = await user.findOne({ email });
+
+    const result = await order.create({ 
+      name,
+      userId: getUserId.id,
+      address,
+      phone,
+      methodPayment,
+      orders
+    })
+
+    return result;
+  }
+}
+
+
+module.exports = OrdersLateMajorityServices;
