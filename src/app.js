6ef9b353536154cde4ab6d9fabd2fa390122ad6b
Jonathan Santos <jonathan01347@gmail.com>
2023-03-18 16:29:17 -0300

    feat(cors,swagger)renderizado end point swagger, e cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
+const swagger = require('swagger-ui-express');
+const swaggerFile = require('./doc/swagger.json');
 const loginRouter = require('./routes/RouteLogin');
 const productsRouter = require('./routes/RouteProducts');
 const mvpRoute = require('./routes/RouteMVP');
@@ -9,7 +12,9 @@ const earlyMajorityRoute = require('./routes/RouteEarlyMajority');
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
+app.use('/docs', swagger.serve, swagger.setup(swaggerFile));
 app.use('/', loginRouter);
 app.use('/', productsRouter);
 app.use('/', mvpRoute);
@@ -18,6 +23,7 @@ app.use('/', earlyMajorityRoute);
 
 app.use((err, _req, res, _next) => {
   const [code, message] = err.message.split('|');
+  console.error(err);
   res.status(Number(code)).json({ message });
 })
 
