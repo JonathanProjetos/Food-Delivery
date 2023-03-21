6b08c38a0305e64d943ce9012b661f6912c4edf9
Jonathan Santos <jonathan01347@gmail.com>
2023-03-21 20:57:01 -0300

    feat(updateOrder)create route for update order.

--- src/routes/RouteMVP.js
+++ src/routes/RouteMVP.js
@@ -8,5 +8,6 @@ MVPRoute.post('/mvp/order', checkToken.verifyToken, ordersMVPControllers.create)
 MVPRoute.get('/mvp/order', checkToken.verifyToken, ordersMVPControllers.getOrders);
 MVPRoute.delete('/mvp/order/:id', checkToken.verifyToken, ordersMVPControllers.deleteOrder);
 MVPRoute.delete('/mvp/order/product/:id', checkToken.verifyToken, ordersMVPControllers.deleteProductOrder);
+MVPRoute.put('/mvp/order/:id', checkToken.verifyToken, ordersMVPControllers.updateOrder);
 
 module.exports = MVPRoute;
