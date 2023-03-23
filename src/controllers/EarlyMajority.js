68852c7b082d2252036ac92a2dba970417ac27c3
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 18:18:59 -0300

    feat(earlyMajority) added method to delete product in orders in controller.

--- src/controllers/EarlyMajority.js
+++ src/controllers/EarlyMajority.js
@@ -15,6 +15,23 @@ const ordersEarlyMajorityController = {
     res.status(200).json(getOrder);
   },
 
+  deleteOrder: async (req, res) => {
+    const { email } = req.email;
+    const { id } = req.params;
+    const deleteOrder = await earlyMajority.deleteOrder({ email, id });
+    res.status(200).json(deleteOrder);
+  },
+
+  deleteProductOrder: async (req, res) => {
+    const { email } = req.email;
+    const { id } = req.params;
+
+    const productId = id;
+
+    const deleteProductOrder = await earlyMajority.deleteProductOrder({ email, productId });
+    res.status(200).json(deleteProductOrder);
+  },
+
 };
 
 module.exports = ordersEarlyMajorityController;
