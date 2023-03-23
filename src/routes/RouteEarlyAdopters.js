d1446fc76f560211ca5cad9311c0af3b2e50641d
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 16:50:58 -0300

    feat(deleteProductOrder) Create route for deleteProductOrder

--- src/routes/RouteEarlyAdopters.js
+++ src/routes/RouteEarlyAdopters.js
@@ -7,5 +7,6 @@ const EarlyAdoptersRoute = Router();
 EarlyAdoptersRoute.post('/earlyAdopters/order', checkToken.verifyToken, ordersEarlyAdoptersControllers.create);
 EarlyAdoptersRoute.get('/earlyAdopters/order', checkToken.verifyToken, ordersEarlyAdoptersControllers.getOrders);
 EarlyAdoptersRoute.delete('/earlyAdopters/order/:id', checkToken.verifyToken, ordersEarlyAdoptersControllers.deleteOrder);
+EarlyAdoptersRoute.delete('/earlyAdopters/order/product/:id', checkToken.verifyToken, ordersEarlyAdoptersControllers.deleteProductOrder);
 
 module.exports = EarlyAdoptersRoute;
