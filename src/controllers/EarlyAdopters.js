324399d17c3f32d39796401a4da18fdeca6c90c7
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 15:19:56 -0300

    feat(EarlyAdopters)Add getOrders in controller.

--- src/controllers/EarlyAdopters.js
+++ src/controllers/EarlyAdopters.js
@@ -9,6 +9,12 @@ const ordersEarlyAdoptersController = {
     res.status(200).json(newOrder);
   },
 
+  getOrders: async (req, res) => {
+    const { email } = req.email;
+    const getOrder = await earlyAdopters.getOrders(email);
+    res.status(200).json(getOrder);
+  },
+
 };
 
 module.exports = ordersEarlyAdoptersController;
