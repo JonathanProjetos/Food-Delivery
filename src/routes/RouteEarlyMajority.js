d9130633578dfa0ecfa73465ce0c3662687445bf
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 18:19:58 -0300

    feat(earlyMajority)Create route for deleteProductOrder.

--- src/routes/RouteEarlyMajority.js
+++ src/routes/RouteEarlyMajority.js
@@ -6,5 +6,7 @@ const EarlyMajorityRoute = Router();
 
 EarlyMajorityRoute.post('/earlyMajority/order', checkToken.verifyToken, ordersEarlyMajorityControllers.create);
 EarlyMajorityRoute.get('/earlyMajority/order', checkToken.verifyToken, ordersEarlyMajorityControllers.getOrders);
+EarlyMajorityRoute.delete('/mvp/order/:id', checkToken.verifyToken, ordersEarlyMajorityControllers.deleteOrder);
+EarlyMajorityRoute.delete('/mvp/order/product/:id', checkToken.verifyToken, ordersEarlyMajorityControllers.deleteProductOrder);
 
 module.exports = EarlyMajorityRoute;
