62df3069ac0c54c5e785b13d6362ed973f5de70e
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 19:33:08 -0300

    feat(lateMajority) Create route for deleteOrder

--- src/routes/RouteLateMajority.js
+++ src/routes/RouteLateMajority.js
@@ -6,5 +6,6 @@ const LateMajorityRoute = Router();
 
 LateMajorityRoute.post('/lateMajority/order', checkToken.verifyToken, ordersLateMajorityControllers.create);
 LateMajorityRoute.get('/lateMajority/order', checkToken.verifyToken, ordersLateMajorityControllers.getOrders);
+LateMajorityRoute.delete('/lateMajority/order/:id', checkToken.verifyToken, ordersLateMajorityControllers.deleteOrder);
 
 module.exports = LateMajorityRoute;
