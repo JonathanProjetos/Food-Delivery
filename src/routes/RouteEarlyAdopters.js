e0d58808c26408b4467a10941e5da8f556cd7e93
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 15:20:38 -0300

    feat(EarlyAdopters) Create route for getOrders.

--- src/routes/RouteEarlyAdopters.js
+++ src/routes/RouteEarlyAdopters.js
@@ -5,5 +5,5 @@ const checkToken = require('../middlewares/checkToken');
 const EarlyAdoptersRoute = Router();
 
 EarlyAdoptersRoute.post('/earlyAdopters/orders', checkToken.verifyToken, ordersEarlyAdoptersControllers.create);
-
+EarlyAdoptersRoute.get('/earlyAdopters/order', checkToken.verifyToken, ordersEarlyAdoptersControllers.getOrders);
 module.exports = EarlyAdoptersRoute;
