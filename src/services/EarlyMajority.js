180f7eed9e25f70e6e544b7c5a5b0f44d2d4099f
Jonathan Santos <jonathan01347@gmail.com>
2023-03-23 18:18:19 -0300

    feat(earlyMajority) Added method to delete product in orders in services

--- src/services/EarlyMajority.js
+++ src/services/EarlyMajority.js
@@ -46,6 +46,39 @@ const OrdersEarlyMajorityServices = {
 
     return getOrder;
   },
+
+  deleteOrder: async ({ email, id }) => {
+    const getUser = await user.findOne({ email });
+
+    if (!getUser) throw new Error('404|User not found');
+
+    const getOrderForId = await order.findOne(
+      { userId: getUser.id },
+      { _id: id },
+    );
+
+    if (!getOrderForId) throw new Error('404|Order not found');
+
+    const deleteOrder = await order.deleteOne({ _id: getOrderForId.id });
+
+    return deleteOrder;
+  },
+
+  deleteProductOrder: async ({ email, productId }) => {
+    const getUser = await user.findOne({ email });
+
+    if (!getUser) throw new Error('404|User not found');
+
+    const result = await order.updateMany(
+      { userId: getUser.id },
+      { $pull: { orders: { _id: productId } } },
+    );
+
+    if (result.nModified === 0) throw new Error('404|Product not found');
+
+    return { message: 'Product deleted' };
+  },
+
 };
 
 module.exports = OrdersEarlyMajorityServices;
